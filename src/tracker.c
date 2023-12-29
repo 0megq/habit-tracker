d22198ab608acb31fd64d92b7b0f1094dc178a4e
0megq <nvatanshenas55@gmail.com>
2023-12-29 13:17:21 -0800

    Habit Text Parsing and Display

--- src/tracker.c
+++ src/tracker.c
@@ -1,19 +1,28 @@
 #include "raylib.h"
 #include <stdio.h>
 #include "gui.h"
+#include "tracker.h"
 
-#define MAX_ROWS 10
-
+// Constants
 const CellStyle defaultCellStyle = {
 	.fontSize = 16,
-	.textHAlign = HALIGN_CENTER,
-	.textVAlign = VALIGN_CENTER,
-	.cellWidth = 60,
-	.cellHeight = 40,
+	// .textHAlign = HALIGN_CENTER,
+	// .textVAlign = VALIGN_CENTER,
 	.borderWidth = 1,
-	.borderColor = GOLD,
-	.color = LIGHTGRAY,
+	.borderColor = DARKGRAY,
+	.color = SKYBLUE,
 };
+
+const CellButtonStyle defaultButtonStyle = {
+	.cellStyle = &defaultCellStyle,
+	.hoveredColor = (Color){200, 200, 200, 100},
+	.pressedColor = (Color){80, 80, 80, 100},
+};
+
+const Vector2 defaultCellSize = {80, 40};
+
+// Static declarations
+static void ParseHabitText(const char *text, char name[MAX_HABIT_NAME_LENGTH], char date[MAX_HABIT_DATE_LENGTH], char data[MAX_ENTRIES][MAX_ENTRY_LENGTH], size_t *columns, size_t *rows);
 
 // Program main entry point
 int main(void)
@@ -22,13 +31,23 @@ int main(void)
 	const int screenWidth = 800;
 	const int screenHeight = 450;
 
-	char *const text = LoadFileText("habits/testhabit.txt");
-	UnloadFileText(text);
 	InitWindow(screenWidth, screenHeight, "Habit Tracker");
 
 	SetTargetFPS(60); // Set our game to run at 60 frames-per-second
 	//--------------------------------------------------------------------------------------
 
+	SetTraceLogLevel(LOG_DEBUG);
+
+	char *const text = LoadFileText("habits/testhabit.txt");
+
+	char habitName[MAX_HABIT_NAME_LENGTH] = {};
+	char date[MAX_HABIT_DATE_LENGTH] = {};
+	char habitData[MAX_ENTRIES][MAX_ENTRY_LENGTH] = {};
+	size_t columns, rows;
+	columns = rows = 0;
+	ParseHabitText(text, habitName, date, habitData, &columns, &rows);
+
+	UnloadFileText(text);
 	// Main game loop
 	while (!WindowShouldClose()) // Detect window close button or ESC key
 	{
@@ -38,10 +57,8 @@ int main(void)
 
 		ClearBackground(RAYWHITE);
 
-		DrawCell("hello", 80, 40, &defaultCellStyle);
-		DrawCell("goop", 80 + defaultCellStyle.cellWidth - defaultCellStyle.borderWidth, 40, &defaultCellStyle);
-		DrawCell("dooo", 80 + defaultCellStyle.cellWidth - defaultCellStyle.borderWidth, 40 + defaultCellStyle.cellHeight - defaultCellStyle.borderWidth, &defaultCellStyle);
-		DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+		DrawText(habitName, 20, 20, 20, DARKGRAY);
+		DrawTable(habitData, columns, rows, (Vector2){70, 70}, defaultCellSize, defaultButtonStyle);
 
 		EndDrawing();
 		//----------------------------------------------------------------------------------
@@ -53,4 +70,82 @@ int main(void)
 	//--------------------------------------------------------------------------------------
 
 	return 0;
+}
+
+static void ParseHabitText(const char *text, char name[MAX_HABIT_NAME_LENGTH], char date[MAX_HABIT_DATE_LENGTH], char data[MAX_ENTRIES][MAX_ENTRY_LENGTH], size_t *columns, size_t *rows)
+{
+	bool nameSet, dateSet, dataSet, columnsSet;
+	nameSet = dateSet = dataSet = columnsSet = false;
+
+	size_t currentEntry = 0;
+	size_t entryIndex = 0;
+
+	size_t colCounter = 0;
+
+	while (*text != '\0')
+	{
+		if (!nameSet)
+		{
+			if (*text == '\n')
+			{
+				*name = '\0';
+				nameSet = true;
+				if (text[1] != '\n')
+				{
+					TraceLog(LOG_ERROR, "ParseHabitText Error: Expected new line, but got %c", text[1]);
+					return;
+				}
+				text++;
+			}
+			*name++ = *text;
+		}
+		else if (!dateSet)
+		{
+			if (*text == '\n')
+			{
+				*date = '\0';
+				dateSet = true;
+				if (text[1] == '\n')
+				{
+					TraceLog(LOG_ERROR, "ParseHabitText Error: Expected start of entry, but got new line");
+					return;
+				}
+			}
+			*date++ = *text;
+		}
+		else if (!dataSet)
+		{
+			if (*text == ',')
+			{
+				colCounter++;
+				data[currentEntry][entryIndex] = '\0';
+				entryIndex = 0;
+				currentEntry++;
+			}
+			else if (*text == '\n')
+			{
+				(*rows)++;
+				if (!columnsSet)
+				{
+					columnsSet = true;
+					*columns = colCounter;
+				}
+				if (colCounter != *columns)
+				{
+					TraceLog(LOG_ERROR, "ParseHabitText Error: Expected %llu columns before new line, but got %llu columns", columns, colCounter);
+					return;
+				}
+				colCounter = 0;
+				if (text[1] == '\n')
+				{
+					dataSet = true;
+				}
+			}
+			else
+			{
+				data[currentEntry][entryIndex++] = *text;
+			}
+		}
+		text++;
+	}
 }
---
+++ src/tracker.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define MAX_HABIT_NAME_LENGTH 20
+#define MAX_HABIT_DATE_LENGTH 10
+#define MAX_ENTRY_LENGTH 20
+#define MAX_ENTRIES 200
