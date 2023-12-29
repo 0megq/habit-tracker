cc3c844bb909e0916b063dfbf13e60144428db4c
0megq <nvatanshenas55@gmail.com>
2023-12-29 13:16:21 -0800

    Gui with button, table, and cells working

--- src/gui.c
+++ src/gui.c
@@ -1,15 +1,84 @@
 #include "raylib.h"
 #include "gui.h"
+#include "tracker.h"
+#include <stdio.h>
+#include <string.h>
 
-void DrawCell(const char *text, int posX, int posY, const CellStyle *style)
+// Static constants
+const char checked[] = "x";
+const char unchecked[] = " ";
+
+// Static variables
+static Vector2 lastPressedPos = {0};
+
+void DrawCell(const char *text, Vector2 pos, Vector2 size, const CellStyle *style)
 {
 	// Draw Cell Rectangle
-	DrawRectangle(posX, posY, style->cellWidth, style->cellHeight, style->color);
-	DrawRectangleLinesEx((Rectangle){posX, posY, style->cellWidth, style->cellHeight}, style->borderWidth, style->borderColor);
+	DrawRectangleV(pos, size, style->color);
+	DrawRectangleLinesEx((Rectangle){pos.x, pos.y, size.x, size.y}, style->borderWidth, style->borderColor);
 
 	// Draw Cell Text
 	int textWidth = MeasureText(text, style->fontSize);
-	int textX = posX + (style->cellWidth - textWidth) / 2;
-	int textY = posY + (style->cellHeight - style->fontSize) / 2;
+	int textX = pos.x + (size.x - textWidth) / 2;
+	int textY = pos.y + (size.y - style->fontSize) / 2;
 	DrawText(text, textX, textY, style->fontSize, BLACK);
+}
+
+ButtonStatus DrawCellButton(const char *text, Vector2 pos, Vector2 size, CellButtonStyle style, Vector2 lastPosPressed)
+{
+	// Draw Cell
+	DrawCell(text, pos, size, style.cellStyle);
+
+	// Draw Button
+	Rectangle cellRec = (Rectangle){pos.x, pos.y, size.x, size.y};
+	return DrawButton(cellRec, BLANK, style.hoveredColor, style.pressedColor, lastPosPressed);
+}
+
+ButtonStatus DrawButton(Rectangle rec, Color normal, Color hovered, Color pressed, Vector2 lastPosPressed)
+{
+	if (CheckCollisionPointRec(GetMousePosition(), rec))
+	{
+		bool wasPressedLast = CheckCollisionPointRec(lastPosPressed, rec);
+		if (IsMouseButtonDown(MOUSE_BUTTON_LEFT) && wasPressedLast)
+		{
+			DrawRectangleRec(rec, pressed);
+			return PRESSED;
+		}
+		else if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT) && wasPressedLast)
+		{
+			return RELEASED;
+		}
+		DrawRectangleRec(rec, hovered);
+		return HOVERED;
+	}
+	DrawRectangleRec(rec, normal);
+	return NORMAL;
+}
+
+void DrawTable(char tableData[MAX_ENTRIES][MAX_ENTRY_LENGTH], size_t columns, size_t rows, Vector2 pos, Vector2 cellSize, CellButtonStyle style)
+{
+	if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+	{
+		lastPressedPos = GetMousePosition();
+	}
+	for (int col = 0; col < columns; col++)
+	{
+		for (int row = 0; row < rows; row++)
+		{
+			int index = col + row * columns;
+			char *text = tableData[index];
+			Vector2 cellPos = (Vector2){pos.x + col * cellSize.x, pos.y + row * cellSize.y};
+			if (TextIsEqual(text, unchecked) || TextIsEqual(text, checked))
+			{
+				if (RELEASED == DrawCellButton(text, cellPos, cellSize, style, lastPressedPos))
+				{
+					TextCopy(text, TextIsEqual(text, unchecked) ? checked : unchecked);
+				}
+			}
+			else
+			{
+				DrawCell(text, cellPos, cellSize, style.cellStyle);
+			}
+		}
+	}
 }
--- src/gui.h
+++ src/gui.h
@@ -1,32 +1,50 @@
 #pragma once
+#include <stdio.h>
+#include "tracker.h"
 
 // Enums
-typedef enum VAlign
-{
-	VALIGN_TOP,
-	VALIGN_CENTER,
-	VALIGN_BOTTOM
-} VAlign;
+// typedef enum VAlign
+// {
+// 	VALIGN_TOP,
+// 	VALIGN_CENTER,
+// 	VALIGN_BOTTOM
+// } VAlign;
 
-typedef enum HAlign
+// typedef enum HAlign
+// {
+// 	HALIGN_LEFT,
+// 	HALIGN_CENTER,
+// 	HALIGN_RIGHT
+// } HAlign;
+
+typedef enum ButtonStatus
 {
-	HALIGN_LEFT,
-	HALIGN_CENTER,
-	HALIGN_RIGHT
-} HAlign;
+	NORMAL,
+	HOVERED,
+	PRESSED,
+	RELEASED,
+} ButtonStatus;
 
 // Structs
 typedef struct CellStyle
 {
 	int fontSize;
-	HAlign textHAlign;
-	VAlign textVAlign;
-	int cellHeight;
-	int cellWidth;
+	// HAlign textHAlign;
+	// VAlign textVAlign;
 	int borderWidth;
 	Color borderColor;
 	Color color;
 } CellStyle;
 
+typedef struct CellButtonStyle
+{
+	const CellStyle *cellStyle;
+	Color hoveredColor;
+	Color pressedColor;
+} CellButtonStyle;
+
 // Function Declarations
-void DrawCell(const char *text, int posX, int posY, const CellStyle *style);
+void DrawCell(const char *text, Vector2 pos, Vector2 size, const CellStyle *style);
+ButtonStatus DrawCellButton(const char *text, Vector2 pos, Vector2 size, CellButtonStyle style, Vector2 lastPosPressed);
+ButtonStatus DrawButton(Rectangle rec, Color normal, Color hovered, Color pressed, Vector2 lastPosPressed);
+void DrawTable(char tableData[MAX_ENTRIES][MAX_ENTRY_LENGTH], size_t columns, size_t rows, Vector2 pos, Vector2 cellSize, CellButtonStyle style);
